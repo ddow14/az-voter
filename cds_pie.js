7ff3d4cf4874feefaf65abc417f836edef499298
ddow14 <63304180+ddow14@users.noreply.github.com>
2022-08-31 15:47:37 -0700

    Update cds_pie.js

--- cds_pie.js
+++ cds_pie.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
-var width = 550
-    height = 550
+var width = 300
+    height = 300
     margin = 40
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
