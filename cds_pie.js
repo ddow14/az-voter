99b94e405366c3341e5c9e19de5cf988a9e796b8
ddow14 <63304180+ddow14@users.noreply.github.com>
2022-08-31 16:34:43 -0700

    Update cds_pie.js

--- cds_pie.js
+++ cds_pie.js
@@ -1,6 +1,12 @@
+function drawChart() {
+
+// get the current width of the div where the chart appear, and attribute it to Svg
+  currentWidth = parseInt(d3.select('#pie').style('width'), 10)
+  Svg.attr("width", currentWidth)
+
 // set the dimensions and margins of the graph
-var width = 300
-    height = 300
+var width = currentWidth
+    height = currentWidth
     margin = 40
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
